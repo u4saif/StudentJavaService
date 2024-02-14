3f1408da02a2b9ce43d9a4ebaee3ad327eab70f8
eysaif <u4saif@gmail.com>
2024-02-14 21:39:52 +0530

    Added Exception Handler

--- src/main/java/com/students/StudentService/rest/StudentController.java
+++ src/main/java/com/students/StudentService/rest/StudentController.java
@@ -1,11 +1,11 @@
 package com.students.StudentService.rest;
 
+import com.students.StudentService.entity.StudentErrorResponse;
 import com.students.StudentService.entity.Students;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +29,27 @@ public class StudentController {
 
     @GetMapping("{studentId}")
     public Students getStudentWithID(@PathVariable int studentId){
+        if(studentId >= theStudents.size() || (studentId < 0)){
+            throw new StudentException("please mention a valid ID. "+studentId+" is not valid!");
+        }
         return theStudents.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> hendleException(StudentException exc){
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+}
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> hendleException(Exception exc){
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
     }
 
 }
---
+++ src/main/java/com/students/StudentService/rest/StudentException.java
@@ -0,0 +1,8 @@
+package com.students.StudentService.rest;
+
+public class StudentException extends RuntimeException{
+
+    public  StudentException(String message){
+        super(message);
+    }
+}
